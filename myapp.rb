97346034e62c21634a69a172ada35908364e8563
Coralie <ruiz.coralie64@live.fr>
2015-02-22 21:48:18 +0100

    Create erb files

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,17 @@
 # mon_application.rb
 require 'sinatra'
+@pseudo = ""
 
 get '/' do
-  'Bonjour le monde !'
+  erb :index
+end
+
+post '/' do
+	pseudo = params[:pseudo]
+	@pseudo = pseudo
+	if @pseudo != ""
+		erb	:chatView
+	else
+		erb :index
+  	end  	
 end
